db6971bc35abf8c6a6e584cf646f5d4307605da5
Amane Suzuki <amane.suzu@gmail.com>
2018-02-16 19:16:21 +0900

    Solve C

--- abc079/C_train_ticker.kt
+++ abc079/C_train_ticker.kt
@@ -1,14 +1,12 @@
-import java.util.*
-
 fun main(args: Array<String>) {
-    var sc = Scanner(System.`in`)
     var (a, b, c, d) = readLine()!!.map { it.toString().toInt() }
     for (op1 in arrayOf(-1, 1)) {
-        var t = a
-        t += (op1)*b
         for (op2 in arrayOf(-1, 1)) {
             for (op3 in arrayOf(-1, 1)) {
-
+                if (a + op1 * b + op2 * c + op3 * d == 7) {
+                    println("$a${if (op1 == 1) "+" else "-"}$b${if (op2 == 1) "+" else "-"}$c${if (op3 == 1) "+" else "-"}$d=7")
+                    return
+                }
             }
         }
     }

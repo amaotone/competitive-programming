3c3c026f0a783dea545b74550376f96cfbff664b
Amane Suzuki <amane.suzu@gmail.com>
2018-02-26 15:29:30 +0900

    ABC079_D :v:

---
+++ abc079/D_wall.py
@@ -0,0 +1,16 @@
+H, W = map(int, input().split())
+cost = [list(map(int, input().split())) for _ in range(10)]
+wall = [list(map(int, input().split())) for _ in range(H)]
+
+for k in range(10):
+    for i in range(10):
+        for j in range(10):
+            cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j])
+
+ans = 0
+for i in range(H):
+    for j in range(W):
+        if wall[i][j] != -1:
+            ans += cost[wall[i][j]][1]
+
+print(ans)

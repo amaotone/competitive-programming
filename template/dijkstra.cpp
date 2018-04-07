1f282b1fd8c7d1b62a3dfea9c2aad2d102ebd313
Amane Suzuki <amane.suzu@gmail.com>
2018-04-08 02:24:43 +0900

    Update template

--- template/dijkstra.cpp
+++ template/dijkstra.cpp
@@ -1,29 +1,68 @@
-using Weight = ll;
-struct Edge{
-    int to; 
-    Weight cost;
-    Edge(int to, Weight cost):to(to),cost(cost){}
-};
+// TEMPLATE
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using P = pair<int, int>;
+#define ALL(a) (a).begin(), (a).end()
+#define FOR(i, a, b) for (int i=(a);(i)<(b);(i)++)
+#define REP(i, n) for (int i=0;(i)<(n);(i)++)
+#define REPS(i, n) for (int i=1;(i)<=(n);(i)++)
+#define RREP(i, n) for (int i=(x-1);(i)>=0;(i)--)
+#define RREPS(i, n) for (int i=n;(i)>0;(i)--)
+#define print(x) (cout<<(x)<<"\n")
+#define INF (1<<29)
+
+//--------------------------------------------------
+// ダイクストラ
+
+using Weight = int;
+struct Edge{ int to; Weight cost; };
 using Graph = vector<vector<Edge>>;
 
-vector<Weight> dijkstra(const Graph &G, int s) {
-    // P=(cost, vertex)
+vector<Weight> dijkstra(Graph& G, int s) {
+    using P = pair<Weight, int>;
+    vector<Weight> d(G.size(), INF);
     priority_queue<P, vector<P>, greater<P>> que;
-    int V = G.size();
-    vector<Weight> d(V, INF);
+    fill(ALL(d), INF);
     d[s] = 0;
-    que.push(P(0, s));
-    while (not que.empty()) {
+    que.push(P{0, s});
+ 
+    while(!que.empty()) {
         P p = que.top(); que.pop();
         int v = p.second;
-        if (d[v] < p.first) continue;
-        REP(i, G[v].size()) {
+        if(d[v] < p.first) {
+            continue;
+        }
+        for(int i=0; i<G[v].size(); ++i) {
             Edge e = G[v][i];
-            if (d[e.to] > d[v] + e.cost) {
+            if(d[e.to] > d[v] + e.cost) {
                 d[e.to] = d[v] + e.cost;
-                que.push(P(d[e.to], e.to));
+                que.push(P{d[e.to], e.to});
             }
         }
     }
     return d;
+}
+
+
+
+// TEST
+void AOJ_GRL_1_A() {
+    int V, E, r; cin>>V>>E>>r;
+    Graph G(V);
+    REP(i, E) {
+        int s, t; Weight d; cin>>s>>t>>d;
+        G[s].push_back((Edge){t, d});
+    }
+    auto d = dijkstra(G, r);
+    REP(i, V) {
+        if (d[i]==INF) cout<<"INF"<<endl;
+        else cout<<d[i]<<endl;
+    }
+} 
+
+signed main() {
+    AOJ_GRL_1_A();
 }
--- template/main.cpp
+++ template/main.cpp
@@ -1,3 +1,4 @@
+// TEMPLATE
 #include <algorithm>
 #include <iostream>
 #include <set>
@@ -12,22 +13,22 @@ typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
 template<class T>bool chmax(T &a, const T &b) {if(a<b) {a=b; return 1;} return 0;}
 template<class T>bool chmin(T &a, const T &b) {if(b<a) {a=b; return 1;} return 0;}
-
 #define ALL(a) (a).begin(), (a).end()
-#define FOR(i, a, b) for (int i=a;(i)<(b);(i)++)
+#define FOR(i, a, b) for (int i=(a);(i)<(b);(i)++)
 #define REP(i, n) for (int i=0;(i)<(n);(i)++)
 #define REPS(i, n) for (int i=1;(i)<=(n);(i)++)
 #define RREP(i, n) for (int i=(x-1);(i)>=0;(i)--)
 #define RREPS(i, n) for (int i=n;(i)>0;(i)--)
-#define gcd __gcd
 #define len(x) ((int)(x).size())
 #define print(x) (cout<<(x)<<"\n")
+const int dx[4]={1,0,-1,0};
+const int dy[4]={0,1,0,-1};
+const int dx8[8]={1,0,-1,1,-1,1,0,-1};
+const int dy8[8]={1,1,1,0,0,-1,-1,-1};
 
-int dx[4]={1,0,-1,0};
-int dy[4]={0,1,0,-1};
-int dx8[8]={1,0,-1,1,-1,1,0,-1};
-int dy8[8]={1,1,1,0,0,-1,-1,-1};
+// LIBRARY
 
+// SOLVE
 signed main() {
     cin.tie(0);
    	ios::sync_with_stdio(false);

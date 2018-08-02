526198178b519141d2b9bad9a8cb6780c324fb07
Amane Suzuki <amane.suzu@gmail.com>
2018-08-03 01:26:46 +0900

    Solve ARC074 C

---
+++ AtCoder/ARC074/c.rs
@@ -0,0 +1,69 @@
+#![allow(dead_code, unused_imports, non_snake_case)]
+
+use std::io::*;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::str::FromStr;
+
+const MOD: i32 = 1_000_000_007;
+
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+    };
+}
+
+fn solve(h: i64, w: i64) -> i64 {
+    if w % 3 == 0 {
+        return 0;
+    }
+    let mut ans = h;
+    for i in 1..w {
+        let a = h * i;
+        let b = (h / 2) * (w - i);
+        let c = (h - h / 2) * (w - i);
+        ans = min(ans, max((a - b).abs(), max((b - c).abs(), (c - a).abs())));
+    }
+    ans
+}
+
+fn main() {
+    let (h, w) = get!(i64, i64);
+    println!("{}", min(solve(h, w), solve(w, h)));
+}

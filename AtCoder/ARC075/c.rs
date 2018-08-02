1436745dc9837c0d2b0e469775d90c5ce01a23aa
Amane Suzuki <amane.suzu@gmail.com>
2018-08-03 00:46:36 +0900

    Rustgit status

---
+++ AtCoder/ARC075/c.rs
@@ -0,0 +1,69 @@
+#![allow(unused_imports, dead_code, non_snake_case)]
+
+use std::io::*;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::str::FromStr;
+
+const MOD: i32 = 1_000_000_007;
+
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let N = get!(usize);
+    let mut v = Vec::new();
+    for _ in 0..N {
+        v.push(get!(usize));
+    }
+    let sum = v.iter().sum();
+    let ans = if sum % 10 != 0 {
+        sum
+    } else {
+        if let Some(x) = v.iter().filter(|&x| x % 10 != 0).min() {
+            sum - x
+        } else {
+            0
+        }
+    };
+    println!("{}", ans);
+}
---
+++ AtCoder/ARC076/c.rs
@@ -0,0 +1,68 @@
+#![allow(unused_imports, non_snake_case)]
+
+use std::io::*;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::str::FromStr;
+
+const MOD: i32 = 1_000_000_007;
+
+macro_rules! get {
+    ($t:ty) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.trim().parse::<$t>().unwrap()
+        }
+    };
+    ($($t:ty),*) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            (
+                $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+            )
+        }
+    };
+    ($t:ty; $n:expr) => {
+        (0..$n).map(|_|
+            get!($t)
+        ).collect::<Vec<_>>()
+    };
+    ($($t:ty),*; $n:expr) => {
+        (0..$n).map(|_|
+            get!($($t),*)
+        ).collect::<Vec<_>>()
+    };
+    ($t:ty ;;) => {
+        {
+            let mut line: String = String::new();
+            std::io::stdin().read_line(&mut line).unwrap();
+            line.split_whitespace()
+                .map(|t| t.parse::<$t>().unwrap())
+                .collect::<Vec<_>>()
+        }
+    };
+    ($t:ty ;; $n:expr) => {
+        (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+    };
+}
+
+fn main() {
+    let (n, m) = get!(i64, i64);
+    if (n - m).abs() > 1 {
+        println!("0");
+        return;
+    }
+    let mut fact: Vec<i64> = vec![1; 100001];
+    for i in 1..fact.len() {
+        let j = i as i64;
+        fact[i] = (fact[i - 1] * j) % (MOD as i64);
+    }
+    let mut ans = (fact[n as usize] * fact[m as usize]) % (MOD as i64);
+    if n == m {
+        ans = ans * 2 % (MOD as i64);
+    }
+    println!("{}", ans);
+}

1f1cb26576da7edd1cd0d6323327ed8e6d3a7779
Amane Suzuki <amane.suzu@gmail.com>
2018-09-08 01:33:08 +0900

    Solve ARC102

---
+++ AtCoder/ARC102/b.rs
@@ -0,0 +1,76 @@
+#![allow(dead_code, unused_imports, non_snake_case)]
+
+use std::io::*;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::str::FromStr;
+
+const MOD: i32 = 1_000_000_007;
+
+fn main() {
+    let (x1, y1, x2, y2): (i32, i32, i32, i32) = input();
+    let (dx, dy) = (x2 - x1, y2 - y1);
+    let (x3, y3) = (x2 - dy, y2 + dx);
+    let (x4, y4) = (x3 - dx, y3 - dy);
+    println!("{} {} {} {}", x3, y3, x4, y4);
+}
+
+trait Read {
+    fn read(s: &str) -> Self;
+}
+
+fn input<T: Read>() -> T {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    T::read(buf.trim_right())
+}
+
+macro_rules! read_impl {
+    ($($t:ty)*) => ($(
+        impl Read for $t {
+            fn read(s: &str) -> $t{
+                s.parse().unwrap()
+            }
+        }
+    )*)
+}
+read_impl! { usize u8 u16 u32 u64 isize i8 i16 i32 i64 f32 f64 }
+
+impl Read for String {
+    fn read(s: &str) -> Self {
+        s.to_string()
+    }
+}
+
+impl Read for Vec<char> {
+    fn read(s: &str) -> Self {
+        s.chars().collect()
+    }
+}
+
+impl<T: Read> Read for Vec<T> {
+    fn read(s: &str) -> Self {
+        s.split_whitespace().map(T::read).collect()
+    }
+}
+
+impl<A: Read, B: Read> Read for (A, B) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]))
+    }
+}
+
+impl<A: Read, B: Read, C: Read> Read for (A, B, C) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]))
+    }
+}
+
+impl<A: Read, B: Read, C: Read, D: Read> Read for (A, B, C, D) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]), D::read(tokens[3]))
+    }
+}
---
+++ AtCoder/ARC102/c.rs
@@ -0,0 +1,79 @@
+#![allow(dead_code, unused_imports, non_snake_case)]
+
+use std::io::*;
+use std::cmp::{max, min, Ordering};
+use std::collections::*;
+use std::str::FromStr;
+
+const MOD: i32 = 1_000_000_007;
+
+fn main() {
+    let (n, k): (i64, i64) = input();
+    let x = n / k;
+    let mut ans = x.pow(3);
+    if k % 2 == 0 {
+        let y = max(n / (k / 2) - x, 0);
+        ans += y.pow(3);
+    }
+    println!("{}", ans);
+}
+
+trait Read {
+    fn read(s: &str) -> Self;
+}
+
+fn input<T: Read>() -> T {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    T::read(buf.trim_right())
+}
+
+macro_rules! read_impl {
+    ($($t:ty)*) => ($(
+        impl Read for $t {
+            fn read(s: &str) -> $t{
+                s.parse().unwrap()
+            }
+        }
+    )*)
+}
+read_impl! { usize u8 u16 u32 u64 isize i8 i16 i32 i64 f32 f64 }
+
+impl Read for String {
+    fn read(s: &str) -> Self {
+        s.to_string()
+    }
+}
+
+impl Read for Vec<char> {
+    fn read(s: &str) -> Self {
+        s.chars().collect()
+    }
+}
+
+impl<T: Read> Read for Vec<T> {
+    fn read(s: &str) -> Self {
+        s.split_whitespace().map(T::read).collect()
+    }
+}
+
+impl<A: Read, B: Read> Read for (A, B) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]))
+    }
+}
+
+impl<A: Read, B: Read, C: Read> Read for (A, B, C) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]))
+    }
+}
+
+impl<A: Read, B: Read, C: Read, D: Read> Read for (A, B, C, D) {
+    fn read(s: &str) -> Self {
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        (A::read(tokens[0]), B::read(tokens[1]), C::read(tokens[2]), D::read(tokens[3]))
+    }
+}
